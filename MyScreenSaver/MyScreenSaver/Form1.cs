7e78cdff7ae84da747a28f3424a3e7b27abcac13
tayala <tayala68@yahoo.com>
2019-10-07 19:56:24 -0700

    Project 3 - Add form to the project

---
+++ MyScreenSaver/MyScreenSaver/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyScreenSaver
+{
+    public partial class formScreenSaver : Form
+    {
+        public formScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        private void formScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            //press any key to close screen sceen saver
+            Close();
+        }
+    }
+}
