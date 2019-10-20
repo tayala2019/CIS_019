696599aaf1cb2d3ed24f4f4cd0d5217ec343e59e
tayala <tayala68@yahoo.com>
2019-10-20 06:24:51 -0700

    Project 5 - Add

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -24,11 +24,14 @@ namespace PictureViewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
+            //Close the form
             this.Close();
         }
 
         private void setBkgrdColor_Click(object sender, EventArgs e)
         {
+            //Show the color dialogbox, if the user clicks OK, change the 
+            //PictureBox control's background to the color the user chooses.
             if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.BackColor= colorDialog1.Color;
@@ -38,11 +41,14 @@ namespace PictureViewer
 
         private void clearPicture_Click(object sender, EventArgs e)
         {
+            //Clear the picture
             pictureBox1.Image = null;
         }
 
         private void showPicture_Click(object sender, EventArgs e)
         {
+            //Show the Open File Dialog. If the user clicks OK
+            //load the picture that the user choose.
             if (openFileDialog1.ShowDialog()==DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
@@ -51,6 +57,11 @@ namespace PictureViewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
+            /** If the user selects the stretch check box
+             * change the pictureBox's
+             * SizeMode property to "stretch". If the usser clears
+             * the checkbox, change it to "Normal".
+             * */
             if (checkBox1.Checked)
             {
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
