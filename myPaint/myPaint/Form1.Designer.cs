ba147acab1157476bd7f18947f01d540c58c3c0c
tayala <tayala68@yahoo.com>
2019-10-26 09:27:36 -0700

    project 6 - Creating variables and event handler

--- myPaint/myPaint/Form1.Designer.cs
+++ myPaint/myPaint/Form1.Designer.cs
@@ -149,6 +149,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(284, 212);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             // 
             // Form1
             // 
--- myPaint/myPaint/Form1.cs
+++ myPaint/myPaint/Form1.cs
@@ -12,9 +12,28 @@ namespace myPaint
 {
     public partial class Form1 : Form
     {
+        //set bitmap variable with a screen size 1024 x 768
+        Bitmap bmp = new Bitmap(1024,768);
+        //pen color is any of the pallette butoons
+        Pen P = new Pen(Color.Black,5);
+        //set when the left mouse button is pressed/held wil activae a draw
+        bool drawing = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Based on the button that the user presses of these choices will be activated
+            if (drawing)
+            {
+                drawing = false;
+            }
+            else
+            {
+                drawing = true;
+            }
         }
     }
 }
