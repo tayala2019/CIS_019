a2033b2e9def36049e9602ed4e08fa0472de72e1
tayala <tayala68@yahoo.com>
2019-10-30 19:39:23 -0700

    Project 8 - Design the interface Part 2

--- BlueLime/BlueLime/Form1.Designer.cs
+++ BlueLime/BlueLime/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BlueLime
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnBack = new System.Windows.Forms.ToolStripButton();
+            this.btnFwd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.CBO = new System.Windows.Forms.ToolStripComboBox();
+            this.btGo = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnBack,
+            this.btnFwd,
+            this.toolStripSeparator1,
+            this.CBO,
+            this.btGo});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(284, 25);
@@ -49,15 +62,54 @@
             this.webBrowser1.Size = new System.Drawing.Size(284, 236);
             this.webBrowser1.TabIndex = 1;
             // 
-            // Form1
+            // btnBack
+            // 
+            this.btnBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
+            this.btnBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(36, 22);
+            this.btnBack.Text = "Back";
+            // 
+            // btnFwd
+            // 
+            this.btnFwd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnFwd.Image = ((System.Drawing.Image)(resources.GetObject("btnFwd.Image")));
+            this.btnFwd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFwd.Name = "btnFwd";
+            this.btnFwd.Size = new System.Drawing.Size(54, 22);
+            this.btnFwd.Text = "Forward";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // CBO
+            // 
+            this.CBO.Name = "CBO";
+            this.CBO.Size = new System.Drawing.Size(121, 25);
+            // 
+            // btGo
+            // 
+            this.btGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btGo.Image = ((System.Drawing.Image)(resources.GetObject("btGo.Image")));
+            this.btGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(26, 22);
+            this.btGo.Text = "Go";
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.toolStrip1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "frmMain";
+            this.Text = "bluelime";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +119,11 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripButton btnBack;
+        private System.Windows.Forms.ToolStripButton btnFwd;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripComboBox CBO;
+        private System.Windows.Forms.ToolStripButton btGo;
     }
 }
 
