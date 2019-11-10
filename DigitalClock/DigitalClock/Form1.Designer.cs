39dc6cc11a09d8d9147d1cd57728208abfa027a6
tayala <tayala68@yahoo.com>
2019-11-09 16:48:09 -0800

    Project 10 - Create label for seconds

--- DigitalClock/DigitalClock/Form1.Designer.cs
+++ DigitalClock/DigitalClock/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.hourMinuteLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // hourMinuteLabel
@@ -41,12 +42,23 @@
             this.hourMinuteLabel.TabIndex = 0;
             this.hourMinuteLabel.Text = "19 : 25";
             // 
+            // secondsLabel
+            // 
+            this.secondsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.White;
+            this.secondsLabel.Location = new System.Drawing.Point(300, 58);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(274, 80);
+            this.secondsLabel.TabIndex = 1;
+            this.secondsLabel.Text = "47";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(748, 340);
+            this.Controls.Add(this.secondsLabel);
             this.Controls.Add(this.hourMinuteLabel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label hourMinuteLabel;
+        private System.Windows.Forms.Label secondsLabel;
     }
 }
 
